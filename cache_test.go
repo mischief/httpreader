59d6bb46c02e8047ed5c62348ddbd8a970246a9b
mischief <mischief@offblast.org>
2015-08-09 01:40:16 -0700

    test that items put in cache can be retrieved

--- cache_test.go
+++ cache_test.go
@@ -11,6 +11,13 @@ func TestCache(t *testing.T) {
 	for i := 0; i < 150; i++ {
 		b := bytes.Repeat([]byte{byte(i)}, 512)
 		c.put(int64(i*512), b)
+
+		// check for presence
+		buf := c.get(0)
+		if buf == nil {
+			t.Errorf("expected cache block %d got nil", 0, buf)
+			return
+		}
 	}
 
 	// check for eviction
